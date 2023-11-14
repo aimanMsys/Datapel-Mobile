ec8d1df0a35ea125bf56477275c178d9538da24e
Arief <ariefdanish890@gmail.com>
2023-11-14 12:44:07 +0800

    stock-detail

--- src/app/pages/secure/stock-detail/stock-detail.page.ts
+++ src/app/pages/secure/stock-detail/stock-detail.page.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router} from '@angular/router';
+import {  LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-stock-detail',
   templateUrl: './stock-detail.page.html',
   styleUrls: ['./stock-detail.page.scss'],
 })
+
+
 export class StockDetailPage implements OnInit {
 
-  constructor(    private router: Router) { }
+  private loading;
+
+  constructor(   
+    private router: Router,
+    private loadingController: LoadingController) { }
 
   ngOnInit() {
   }
@@ -16,4 +23,40 @@ export class StockDetailPage implements OnInit {
   scan(){
     this.router.navigate(['/stock-lookup']);
   }
+
+  nextPage(){
+    this.router.navigate(['/stock-batchscanner']);
+  }
+
+  home(){
+    this.router.navigate(['/home'])
+  }
+
+  signin(){
+    this.router.navigate(['/signin'])
+  }
+
+  async signout(){
+    const loading = await this.loadingController.create({
+      cssClass: 'default-loading',
+      message: '<p>Signing out...</p><span>See You Again.</span>',
+      spinner: 'crescent'
+    });
+    await loading.present();
+
+    // TODO: Add your sign in logic
+    // ...
+
+    // Fake timeout
+    setTimeout(async () => {
+
+      // Sign in success
+      await this.router.navigate(['/signin']);
+      loading.dismiss();
+    }, 4000);
+  }
+  
 }
+
+
+
