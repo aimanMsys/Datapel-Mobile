a59f6d4ff9872998d9ef031e06aa8ea60071b5ab
Arief <ariefdanish890@gmail.com>
2023-11-14 12:45:36 +0800

    stock-lookup

--- src/app/pages/secure/stock-lookup/stock-lookup.page.ts
+++ src/app/pages/secure/stock-lookup/stock-lookup.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
-import { AlertController } from '@ionic/angular';
+import { AlertController,LoadingController } from '@ionic/angular';
 import { Router } from '@angular/router';
+
+
+
+
+
 // https://ionic.io/blog/how-to-build-an-ionic-barcode-scanner-with-capacitor
 @Component({
   selector: 'app-stock-lookup',
@@ -13,8 +18,12 @@ export class StockLookupPage implements OnInit {
   isSupported = false;
   barcodes: Barcode[] = [];
   barcode:any="";
+ 
 
-  constructor( private router: Router,private alertController: AlertController) {}
+  constructor( 
+    private router: Router,
+    private alertController: AlertController,
+    private loadingController: LoadingController) {}
 
   ngOnInit() {
     BarcodeScanner.isSupported().then((result) => {
@@ -51,5 +60,66 @@ export class StockLookupPage implements OnInit {
     this.router.navigate(['/stock-detail']);
   }
 
+  home(){
+    this.router.navigate(['/home'])
+  }
+
+  signin(){
+    this.router.navigate(['/signin'])
+  }
+  async signout(){
+    const loading = await this.loadingController.create({
+      cssClass: 'default-loading',
+      message: '<p>Signing out...</p><span>See You Again.</span>',
+      spinner: 'crescent'
+    });
+    await loading.present();
+
+    // TODO: Add your sign in logic
+    // ...
+
+    // Fake timeout
+    setTimeout(async () => {
+
+      // Sign in success
+      await this.router.navigate(['/signin']);
+      loading.dismiss();
+    }, 4000);
+  }
+
+  async search(): Promise<void> {
+    if (this.barcode !== '0304') {
+      await this.presentAlert2('System Alert ', 'The item barcode or number was not found.');
+      return; // Exit the function early if the barcode is incorrect
+    }
+  
+    const loading = await this.loadingController.create({
+      cssClass: 'default-loading',
+      message: '<p><ion-icon name="search-circle-outline"></ion-icon> Searching for item </p><span>Please wait...</span> ',
+      spinner: 'crescent'
+    });
+    await loading.present();
+  
+    setTimeout(async () => {
+      await this.router.navigate(['/stock-detail']);
+      loading.dismiss();
+    }, 2000);
+  }
+  
+  async presentAlert2(header: string, message: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'custom-alert', // Apply the custom CSS class
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+  
+  
+
 
 }
+  
+
+
